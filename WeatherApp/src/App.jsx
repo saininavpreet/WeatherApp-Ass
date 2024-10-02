51d892bc635df2a2fc896e39f04fb2504a3dd54a
Navpreet <navpreetsaini1704@gmail.com>
2024-10-02 15:09:51 +0530

    setup

--- WeatherApp/src/App.jsx
+++ WeatherApp/src/App.jsx
@@ -1,33 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
+import SearchResult from './components/SearchResult'
+import Notfound from './components/Notfound'
+import axios from 'axios'
+import Images from './components/Images'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [search, setSearch] = useState('');
+  const [weatherDetails, setWeatherDetails] = useState([]);
+  const [refresh, setRefresh] = useState(false);
+  const [notFound, SetnotFound] = useState(false)
 
+  const searchCity = async () => {
+    try {
+      // const api = import.meta.env.VITE_API_KEY;
+      const url = https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=8be4372635cf8a92cbd484a4e460fce0;
+      const res = await axios.get(url);
+      setWeatherDetails(res.data)
+      setRefresh(true);
+      SetnotFound(false)
+      setSearch("");
+    } catch (error) {
+      if (!search) {
+        alert("Enter city name first!!");
+      }
+      setRefresh(true);
+      SetnotFound(true);
+      return console.log(error.message);
+    }
+  }
+  let bgImage;
+  if (refresh && !notFound) {
+    console.log(weatherDetails.weather[0].main)
+    switch (weatherDetails.weather[0].main) {
+      case "Clear":
+        bgImage = { backgroundImage: url(${Images.clear}) }
+        break;
+      case "Clouds":
+        bgImage = { backgroundImage: url(${Images.clouds}) }
+        break;
+      case "Snow":
+        bgImage = { backgroundImage: url(${Images.snow}) }
+        break;
+      case "Rain":
+        bgImage = { backgroundImage: url(${Images.rain}) }
+        break;
+      case "Drizzle":
+        bgImage = { backgroundImage: url(${Images.drizzle}) }
+        break;
+      case "Thunderstorm":
+        bgImage = { backgroundImage: url(${Images.thunderstorm}) }
+        break;
+    }
+  }
+  if (notFound) {
+    bgImage = { backgroundImage: url(${Images.nf}) }
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className={container} style={bgImage}>
+        <p className="title">Weather App</p>
+        <div className="searchBar">
+          <input
+            type="text"
+            placeholder='Enter your city name...'
+            value={search}
+            onChange={((e) => { setSearch(e.target.value) })}
+          />
+          <button className='searchbtn' onClick={searchCity}>Search</button>
+        </div>
+        {
+          refresh ?
+            (notFound ? (<Notfound />) : (<SearchResult weatherDetails={weatherDetails} />)) :
+            (<p className='bottom-text'>Know your city's weather</p>)
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
